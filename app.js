4dd3f4a1b2b5253592a2069e0ddd56460f8affe8
victor_perezvaldez@hotmail.com <victor_perezvaldez@hotmail.com>
2022-03-24 23:31:18 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function contar(ciudadanos){
+    const map = new Map();
+    for(let i =0; i<ciudadanos.length; i++){
+        if(!map.has(ciudadanos[i])){
+            map.set(ciudadanos[i],1)
+        }else{
+            map.set(ciudadanos[i],map.get(ciudadanos[i]) + 1)
+        }
+    }
+    map.forEach((value,key) => {
+        console.log(key +':' +value);
+    })
+}
+
+contar(['Mexicano','Mexicano','Chile','Chile','Chile'])
